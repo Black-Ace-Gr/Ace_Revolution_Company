848c2cf39738cfffb53874843d7702adfb2a3a50
Black-Ace-Gr <gregorydavid373@gmail.com>
2025-10-27 19:59:27 +0300

    ace revolution

--- ace_revolution_company/settings.py
+++ ace_revolution_company/settings.py
@@ -77,3 +77,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("gregorydavid373@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("tnbc axwg okon nlyt")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
