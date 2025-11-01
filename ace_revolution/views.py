7be234bd90217eb82d2b3a3b372b36bc0f286b53
Black-Ace-Gr <gregorydavid373@gmail.com>
2025-11-01 20:10:10 +0300

    Complete

--- ace_revolution/views.py
+++ ace_revolution/views.py
@@ -27,6 +27,6 @@ def contact(request):
         )
 
         messages.success(request, "Message received! I’ll get back to you soon.")
-        return redirect("contact")
+        return redirect("home")
 
-    return render(request, "ace_revolution_company/contact.html")
+    return render(request, "ace_revolution_company/home.html")
